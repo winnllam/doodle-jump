69e9b2be318d7b08159a705eec929d7f1c08cb19
Winn <winnieee.lam00@gmail.com>
2020-12-01 22:01:56 -0500

    Renamed files

--- doodlejump.s
+++ doodlejump.s
@@ -33,13 +33,7 @@
 
 .data
 
-# Screen 
-screenWidth: 	.word 32
-screenHeight: 	.word 32	# display width in pixels/unit width in pixels
-pixel:		.word 1024
-
 # Display locations
-displayAddress:	.word	0x10008000	# same as $gp
 doodleStart: 	.word	0x10008dc0
 
 # Anmiation delay
@@ -51,136 +45,147 @@ doodleColour:		.word	0x745185 	# purple
 platformColour:		.word	0x74bea7	# green
 
 # Controls (ASCII numbers)
-direction:	.word 0
-left:		.word 74	# j 6A
-right:		.word 75	# k 6B
-start:		.word 73
+left:		.word 0x6A	# j 
+right:		.word 0x6B	# k
+start:		.word 0x73
+keyboardAddress1:	.word	0xffff0000
+keyboardAddress2:	.word	0xffff0004
 
 # Objects
 platforms:	.space 	32	# 4 byte * 8 platforms
 platformLength:	.word 	6
 
+### REGISTERS ###
+# $a2
+# $a3 - current platform
+# $s0 - background colour
+# $s1 - doodle colour
+# $s2 - platform colour
+# $s3 - platform array
+# $s4 - platform length
+# $s5 - doodle start
+# $s6 - doodle position
+# $s7 - 
+# $k0, $k1 - keyboard inputs
+# $gp - display address
 
-.text
 ### INITIALIZATION START ###
+.text
 main:
+	lw $s0, backgroundColour
+	lw $s1, doodleColour
+	lw $s2, platformColour
+	la $s3, platforms
+	lw $s4, platformLength
+	lw $s5, doodleStart
+
 ### Fill background ###
-	lw $a0, pixel		# 1024
-	lw $a1, backgroundColour
-
+backgroundFillInit:
 	li $t0, 0 		# loop counter
-	lw $t1, displayAddress	# base address for display
-
+	add $t1, $gp, $zero
+	
 backgroundFill:
-	sw $a1, 0($t1) 		# save background colour at location
+	sw $s0, 0($t1) 		# save background colour at location
 	add $t1, $t1, 4 	# increment to next pixel
 	addi $t0, $t0, 1
-	bne $t0, $a0, backgroundFill	# 1024
+	bne $t0, 1024, backgroundFill	# 1024 pixels
 	
-
 ### Draw platforms ###
-# TODO: generate new platforms
 initPlatforms:
-	la $s7, platforms	# load space for platform array
-	li $t8, 0		# counter for # of platforms
-	li $t7, 0		# offset shifts for platform array
-	li $t5, 0		# partition for platform locations
+	li $t0, 0		# counter for # of platforms
+	li $t1, 0		# offset shifts for platform array
+	li $t2, 0		# partition for platform locations
 
-platformPrep:
+generateLocation:
 	li $v0, 42		# RNG for platform locations
 	li $a0, 0		# number stored into $a0
 	li $a1, 128		# 1024/4
 	syscall
 	
-	add $a0, $a0, $t5 	# add section offset for platform
-	addi $t5, $t5, 128
+	add $a0, $a0, $t2 	# add section offset for platform
+	addi $t2, $t2, 128
 	
-	add $t6, $s7, $t7
-	addi $t7, $t7, 4	# update platform array offset
-	
-	lw $a3, platformLength
-	lw $t4, platformColour
+	add $t6, $s3, $t1
+	addi $t1, $t1, 4	# update platform array offset
 
-	li $t0, 0 		# loop counter
-	lw $t1, displayAddress	# base address for display
+	li $t5, 0 		# loop counter
 	
-loadPlatformLocation:
-	li $s4, 4		# multiplier
-	mult $s4, $a0		# RNG * 4 (so multiple of 4)
+loadPlatformAddress:
+	li $t4, 4		# multiplier
+	mult $t4, $a0		# RNG * 4 (so multiple of 4)
 	mflo $a1
 	
 	sw $a1, 0($t6)		# store into memory (array)
 	
-	add $t1, $t1, $a1	# load rng location
+	add $t8, $gp, $a1	# load rng location
 
 drawPlatform:
-	sw $t4, 0($t1) 			# save platform colour at location
-	add $t1, $t1, 4 		# increment to next pixel horizontal
-	addi $t0, $t0, 1
-	bne $t0, $a3, drawPlatform	# platform length is 5
+	sw $s2, 0($t8) 			# save platform colour at location
+	add $t8, $t8, 4 		# increment to next pixel horizontal
+	addi $t5, $t5, 1
+	bne $t5, $s4, drawPlatform	# platform length is 5
 	
-	addi $t8, $t8, 1		# increment to next platform
-	bne $t8, 7, platformPrep	# loop to generate another platform (7 times)
-	
-	lw $s0, doodleStart		# base address for doodle starting location
-	addi $s0, $s0, 120		# shift down and left 2 (128 - 4 - 4 = 120)
-	li $t0, 0
+	addi $t0, $t0, 1		# increment to next platform
+	bne $t0, 7, generateLocation	# loop to generate another platform (7 times)
+
+startingPlatformInit:
+	li $a1, 3636			# platform pixel location
+	addi $t6, $s3, 28
+	sw $a1, 0($t6)			# save to last spot in array	
+		
+	addi $t7, $s5, 120		# shift down and left 2 from doodle location (128 - 4 - 4 = 120)
+	li $t5, 0
 	
 drawStartingPlatform:		
-	sw $t4, 0($s0)			# 8th platform initialized to below doodle
-	addi $t0, $t0 1
-	addi $s0, $s0, 4
-	bne $t0, $a3, drawStartingPlatform
+	sw $s2, 0($t7)			# 8th platform initialized to below doodle
+	addi $t5, $t5, 1
+	addi $t7, $t7, 4
+	bne $t5, $s4, drawStartingPlatform
 
 ### Draw doodle ###
 drawDoodle:
-	lw $a1, doodleColour
-	lw $t7, backgroundColour	# save colour at current location
-
-	lw $s0, doodleStart		# base address for display (reset)
-	sw $a1, 0($s0)			# draw initial doodle
-
-startKeyCheck:
-	lw $t0, 0xffff0000
-	bne $t0, 1, startKeyCheck	# no click? loop
+	sw $s1, 0($s5)			# draw initial doodle
 	
-	lw $t2, 0xffff0004		# s is clicked, start game (s, 73)
-	bne $t2, 0x73, startKeyCheck		
+#startKeyCheck:
+	#li $v0, 32		# sleep
+	#li $a0, 100
+	#syscall
+	
+	#lw $k0, 0xffff0000
+	#beq $k0, 1, startKeyCheck
+	
+	#lw $k1, 0xffff0004		# s is clicked, start game
+	#bne $k1, 0x73, startKeyCheck		
 
 ### INITIALIZATION END ###
 
 ### Move Doodle ###
-keyCheckInit:	# start of the jumping process
-	lw $t0, 0xffff0000
-	beq $t0, 1, movementKeyPress
-	
 doodleJumpInit:
-	li $k0, 0
-	li $t9, 0		# counter for distance up and down
+	li $t1, 0		# counter for distance up and down
+	add $t0, $s0, $zero
 
 doodleJumpUp:
-	sub $s0, $s0, 128	# up
-	sw $t7, 128($s0)	
+	sub $s5, $s5, 128	# up
+	sw $t0, 128($s5)	
 	jal moveDoodle
 	
-	addi $t9, $t9, 1
+	addi $t1, $t1, 1
 	
 	li $v0, 32		# sleep to delay animation
 	lw $a0, jumpDelay
 	syscall
 	
 	jal keyCheck
-	
 	jal checkPlatforms
 	
-	bne $t9, 10, doodleJumpUp	# continue up
+	bne $t1, 10, doodleJumpUp	# continue up
 	
 doodleJumpDown:
-	addi $s0, $s0, 128	# down
-	sw $t7, -128($s0)	
+	addi $s5, $s5, 128	# down
+	sw $t0, -128($s5)	
 	jal moveDoodle
 	
-	sub $t9, $t9, 1
+	sub $t1, $t1, 1
 	
 	li $v0, 32		# sleep to delay animation
 	lw $a0, jumpDelay
@@ -189,53 +194,48 @@ doodleJumpDown:
 	jal keyCheck		# check for input while movement to continue
 	jal checkPlatforms
 	
-	bne $t9, 0, doodleJumpDown	# continue down	
-		
-	j keyCheckInit		# back up
+	bge $s5, 0x10009000, Exit
+	
+	j doodleJumpDown	# continue down	
 
 keyCheck:
-	lw $t0, 0xffff0000
-	beq $t0, 1, movementKeyPress
+	lw $k0, 0xffff0000
+	beq $k0, 1, movementKeyPress
 	
 	jr $ra
 
 movementKeyPress:	
-	lw $t2, 0xffff0004
-	beq $t2, 0x73, main	# s for restart
+	lw $k0, 0xffff0004
+	beq $k0, 0x73, main	# s for restart
 	
-	beq $t2, 0x6A, moveDoodleLeft	# j
-	beq $t2, 0x6B, moveDoodleRight	# k
+	beq $k0, 0x6A, moveDoodleLeft	# j
+	beq $k0, 0x6B, moveDoodleRight	# k
 	
-	j keyCheckInit		# not a movement key, continue looping
+	j doodleJumpInit		# not a movement key, continue looping
 
 moveDoodleLeft:
-	#beq $t1, 0x10008000, movementKeyPress	# hit left border
-		
-	sub $s0, $s0, 8		# move left
-	sw $t7, 8($s0)		# load previous colour at current location
+	sub $s5, $s5, 8		# move left
+	sw $t0, 8($s5)		# load previous colour at current location
 	
 	j moveDoodle		# skip over moveDoodleRight
 	
 moveDoodleRight:
-	#beq $t1, 0x1000807c, movementKeyPress	# hit right border
-	
-	add $s0, $s0, 8	
-	sw $t7, -8($s0)	
+	add $s5, $s5, 8	
+	sw $t0, -8($s5)	
 
 moveDoodle:	# colour saving and loading		
-	lw $t7, 0($s0)		# save new colour at location
-	sw $a1, 0($s0)		# load new colour
+	lw $t0, 0($s5)		# save new colour at location
+	sw $s1, 0($s5)		# load new colour
 	
 	jr $ra
-
 
 ### Movement to platforms ###
 checkPlatforms:
 	addi $sp, $sp, -4 	
 	sw $ra, 0($sp)		# add pointer to jump up to stack
 
-	la $t4, platforms
-	li $t8, 0		# initialize platform counter
+	add $t4, $s3, $zero	# load platform location
+	li $t2, 0		# initialize platform counter
 
 	jal checkOnPlatformInit
 	
@@ -247,18 +247,18 @@ checkPlatforms:
 checkOnPlatformInit:
 	lw $t3, 0($t4)		# load platform location
 	add $t3, $t3, $gp	# $gp is same as displayAddress
-	li $t6, 0		# counter for platform length
+	li $t5, 0		# counter for platform length
 
 checkOnPlatform:
-	beq $t3, $s0, onPlatform	# check entire length of platform (length of 6)
+	beq $t3, $s5, onPlatform	# check entire length of platform (length of 6)
 	addi $t3, $t3, 4
 	
-	addi $t6, $t6, 1		# platform length counter
-	bne $t6, 6, checkOnPlatform	
+	addi $t5, $t5, 1		# platform length counter
+	bne $t5, 6, checkOnPlatform	
 	
 	addi $t4, $t4, 4	# increment offset
-	addi $t8, $t8, 1	# incrememnt platform counter
-	bne $t8, 8, checkOnPlatformInit 
+	addi $t2, $t2, 1	# incrememnt platform counter
+	bne $t2, 8, checkOnPlatformInit 
 	
 	j noPlatform
 	
@@ -268,91 +268,75 @@ onPlatform: # TODO: on new platform? if row same as last time its a nope
 	
 	jal backdropShiftInit
 
-	sub $s0, $s0, 128	# jump onto platform
-	lw $t7, backgroundColour
+	sub $s5, $s5, 128	# jump onto platform
+	add $t0, $s0, $zero	# save background colour
+	
+	li $t1, 0		# reset jump counter
+	j doodleJumpUp
 	
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4	# load pointer back to checkPlatform
 	
 	jr $ra
 	
-noPlatform:
-	#addi $sp, $sp, -4 	# save pointer back to checkPlatform
-	#sw $ra, 0($sp)	
-
-	
-continuousDrop:
-	
-	#addi $t6, $t6, 128
-	
-	beq $t1, 0, Exit	# end game if hit bottom of screen
-	
-	#lw $ra, 0($sp)
-	#addi $sp, $sp, 4	# load pointer back to checkPlatform
-	
+noPlatform:	
 	jr $ra
-
+	
+### Background scroll ###
 backdropShiftInit: # recolour the background
-	lw $a0, pixel		# 1024
-	lw $a2, backgroundColour
-
-	li $t0, 0 		# loop counter
-	lw $t1, displayAddress	# base address for display
+	li $t2, 0 		# loop counter
+	add $t3, $gp, $zero	# base address for display
 	
 backdropShift:
-	sw $a2, 0($t1) 		# save background colour at location
-	add $t1, $t1, 4 	# increment to next pixel
-	addi $t0, $t0, 1
-	bne $t0, $a0, backdropShift	# 1024
-	
+	sw $s0, 0($t3) 		# save background colour at location
+	add $t3, $t3, 4 	# increment to next pixel
+	addi $t2, $t2, 1
+	bne $t2, 1024, backdropShift	# 1024
+
 platformShiftInit: 	#s1 2 3
-	la $s1, platforms
-	lw $s2, platformColour
-	li $t8, 0		# initialize platform counter
+	add $t4, $s3, $zero 	# load platform array
+	li $t5, 0		# initialize platform counter
 	
 platformShiftDown:	# shift platforms down and store
-	lw $t3, 0($s1)		# load platform location
-	addi $t3, $t3, 128	# shift down
+	lw $t6, 0($t4)		# load platform location
+	addi $t6, $t6, 128	# shift down
 	
-	bge $t3, 4096, generateTopPlatform
-	# TODO: if new location is out of bounds (> 1024), replace with a regenerated one in the top quadrant
-	sw $t3, 0($s1)		# save new value to array
+	ble $t6, 4096, continuePlatformShift	# check if there is a need to generate top platform
 	
-	addi $s1, $s1, 4	# increment offset to next value in array
-	
-	li $s3, 0		# reset display location
-	add $s3, $gp, $t3
-	
-	li $t6, 0		# initialize platform length counter
-	
-platformShiftRight:	# draw the entire platform
-	sw $s2, 0($s3)		# colour into display
-	addi $s3, $s3, 4	# offset for rest of platform
-	
-	addi $t6, $t6, 1	# increment platform length counter
-	bne $t6, 6, platformShiftRight
-	
-	addi $t8, $t8, 1		# increment platform counter
-	bne $t8, 8, platformShiftDown	# 8 platforms at a time
-	
-	addi $s0, $s0, 128
-	
-	jr $ra
-
-
-generateTopPlatform:
 	li $v0, 42		# RNG for platform locations
 	li $a0, 0		# number stored into $a0
-	li $a1, 128		# 1024/4
+	li $a1, 128		# 1024/8 = 128
 	syscall
 	
-	mult $s4, $a0		# RNG * 4 (so multiple of 4)
-	mflo $t3
+	li $t9, 4
+	mult $t9, $a0
+	mflo $t6
+	
+continuePlatformShift:
+	sw $t6, 0($t4)		# save new value to array
+	
+	addi $t4, $t4, 4	# increment offset to next value in array
+	
+	li $t8, 0		# reset display location
+	add $t8, $gp, $t6
+	
+	li $t7, 0		# initialize platform length counter
+	
+platformShiftRight:	# draw the entire platform
+	sw $s2, 0($t8)		# colour into display
+	addi $t8, $t8, 4	# offset for rest of platform
+	
+	addi $t7, $t7, 1	# increment platform length counter
+	bne $t7, 6, platformShiftRight
+	
+	addi $t5, $t5, 1		# increment platform counter
+	bne $t5, 8, platformShiftDown	# 8 platforms at a time
+	
+	addi $s5, $s5, 128		# shift doodle position
 	
 	jr $ra
-
+	
+	
 Exit:
 	li $v0, 10 # terminate the program gracefully
 	syscall
-
-
